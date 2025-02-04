c470550f7cdd30a33b92f031247e70a45a1b7da2
pgmee <pigmejov@gmail.com>
2025-02-04 10:18:14 +0100

    Init commit

---
+++ src/test/java/org/bromanowski/restcruddemo/RestCrudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bromanowski.restcruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCrudDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
